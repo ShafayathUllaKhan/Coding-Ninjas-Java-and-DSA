19089706a384279c219be4abcf1b1d44a0582e72
Shafayath Ulla Khan <shafayathullakhan1998@gmail.com>
2025-08-02 21:30:33 +0530

    latest commit

---
+++ src/Chapter_1/First_1.java
@@ -0,0 +1,16 @@
+package Chapter_1;
+
+public class First_1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// class first latter name should always be in caps
+		System.out.println("shafayath ulla khan");// ln after printing it goes next line
+		// javac Chapter_1/First_1
+		// java Chapter_1.First_1 
+		// public class will allow acces to jvm 
+		
+		
+	}
+
+}
