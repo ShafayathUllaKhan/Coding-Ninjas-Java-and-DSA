be23d139907828d8025475c5c2c5bcb6b18b7dc7
Shafayath Ulla Khan <shafayathullakhan1998@gmail.com>
2025-11-13 22:40:54 +0530

    latest commit

---
+++ src/Chapter_23/Knapsack.java
@@ -0,0 +1,66 @@
+package Chapter_23;
+
+import java.util.ArrayList;
+
+public class Knapsack {
+
+static ArrayList<ArrayList<Integer>> dp;
+	
+	//Memoization
+	private static int knapsack(int w,int[] val,int[] wt,int idx) {
+		//Base
+		if(idx < 0) return 0;
+		if(dp.get(idx).get(w) != -1) dp.get(idx).get(w);
+		//Choices
+		int take = Integer.MIN_VALUE, not_take;
+		not_take = knapsack(w, val, wt, idx - 1);
+		if(wt[idx] <= w)
+			take = val[idx] + knapsack(w-wt[idx], val, wt, idx - 1);
+		//Optimal
+		int temp = Math.max(take, not_take);
+		dp.get(idx).set(w, temp); 
+		return temp;
+	}
+	
+	//Tabulation
+	private static int knapsackTab(int w,int[] val,int[] wt,int n) {
+		for(int i=0;i<=w;i++) {
+			dp.get(0).set(i, 0);
+			}
+			for(int i=0;i<=n;i++) {
+				dp.get(i).set(0, 0);
+			}
+		for(int i=1;i<=n;i++) {
+			for(int j =1;j<=w;j++) {
+				int take = Integer.MIN_VALUE,not_take;
+				not_take = dp.get(i - 1).get(j);
+				if(wt[i-1] <= j)
+					take = val[i - 1] + dp.get(i-1).get(j - wt[i-1]);
+				int temp = Math.max(take, not_take);
+				dp.get(i).set(j,  temp);
+			}
+		}
+		return dp.get(n).get(w);
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int w= 5;
+		int[] val = {5, 4, 8, 6};
+		int[] wt = {1, 2, 4,5};
+		int n = 4;
+		
+		dp = new ArrayList<>(n + 1);
+		for(int i=0;i<=n; i++) {
+			ArrayList<Integer> row = new ArrayList<>(w + 1);
+			for(int j=0; j<=w;j++) {
+				row.add(-1); //memozition
+			}
+			dp.add(row);
+		}
+		//int ans = knapsack(w, val, wt, n-1);
+		int ans = knapsackTab( w, val, wt, n);
+		System.out.println("Max. profit: "+ ans);
+	}
+	// fractional knapsack
+	// unbounded knapsack
+}
---
+++ src/Chapter_23/UnboundedKnapsack.java
@@ -0,0 +1,51 @@
+package Chapter_23;
+
+import java.util.ArrayList;
+
+public class UnboundedKnapsack {
+
+static ArrayList<ArrayList<Integer>> dp;
+	
+	private static int helper(int[] coins, int amt, int idx) {
+		//Base
+		if(amt == 0) return 0;
+		if(idx < 0) return Integer.MAX_VALUE - 1;
+		
+		if(dp.get(idx).get(amt) != -1) return dp.get(idx).get(amt);
+		
+		// Choices
+		int take = Integer.MAX_VALUE, notTake;
+		
+		notTake = helper(coins, amt, idx-1);
+		if(coins[idx] <= amt) {
+			take = 1 + helper(coins, amt - coins[idx],idx);
+		}
+		
+		//Optimal
+		int temp = Math.min(take, notTake);
+		dp.get(idx).set(amt, temp);
+		return temp;
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int coins[] = {1, 2, 5};
+		int n = coins.length, amt = 11;
+		
+		dp = new ArrayList<>(n + 1);
+		for(int i=0;i<=n; i++) {
+			ArrayList<Integer> row = new ArrayList<>(amt + 1);
+			for(int j=0; j<=amt;j++) {
+				row.add(-1); //memozition
+			}
+			dp.add(row);
+		}
+		
+		int ans = helper(coins, amt , n-1);
+		if(ans == Integer.MAX_VALUE - 1)
+			System.out.println("Not possible");
+			else
+				System.out.println("Minimum coins: " + ans);
+
+	}
+
+}
